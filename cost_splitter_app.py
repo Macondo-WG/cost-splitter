988567cca0147ef5478b187ad7376de0b3141e86
Macondo-WG <macondoooo5@gmail.com>
2025-05-07 00:50:45 +0200

    Update cost_splitter_app.py

--- cost_splitter_app.py
+++ cost_splitter_app.py
@@ -19,10 +19,11 @@ item = st.text_input("Item")
 amount = st.number_input("Amount (€)", min_value=0.0, format="%.2f")
 date = st.date_input("Date", value=datetime.today())
 split_among = st.multiselect('Split among', ["Alice", "Bob", "Carol", "Dave"])
+split_among = ", ".join(split_among)
 
 if st.button("Submit Expense"):
     if name and item and amount:
-        sheet.append_row([name, item, amount,  date.strftime("%Y-%m-%d"), [split_among]])
+        sheet.append_row([name, item, amount,  date.strftime("%Y-%m-%d"), split_among])
         st.success("💾 Cost saved to Google Sheets!")
 
 # Optionally show existing entries
