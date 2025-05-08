b2f693764f44f8ecb329b1bb5ee14e974081ce4b
Macondo-WG <macondoooo5@gmail.com>
2025-05-08 02:00:32 +0200

    interest

--- wg_permanent_expenses.py
+++ wg_permanent_expenses.py
@@ -76,7 +76,7 @@ if username in user_dict: # check authentication
                 years = [round(i.days/365, 2) for i in time_diffs]
                 costs = pd.to_numeric(df_itemdata.cost[mask], errors='coerce')
                 st.write(costs)
-                rest_value_item = costs * np.power(np.ones(len(costs))*(1 - 0.01), years)/no_members
+                rest_value_item = costs * np.power(np.ones(len(costs))*(1 - 0.1), years)/no_members
 
                 rest_value_sum = rest_value_item.sum()
                 
